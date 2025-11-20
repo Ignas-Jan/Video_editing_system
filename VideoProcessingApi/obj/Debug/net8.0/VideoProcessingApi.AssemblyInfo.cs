7c96e90ec9f95ecbee2795a2e1a936d1a12ef2c6
Ignas Janciukas <janciukas777@gmail.com>
2025-11-20 14:36:22 +0200

    Installed all the required packages for video processing api from nuget

--- VideoProcessingApi/obj/Debug/net8.0/VideoProcessingApi.AssemblyInfo.cs
+++ VideoProcessingApi/obj/Debug/net8.0/VideoProcessingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoProcessingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad59d69dc55f9e126583717d1a9859ea2c5f923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20803b4ef94a6b46afd8673f9bbaf89eee636d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoProcessingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoProcessingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
